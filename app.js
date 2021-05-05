1f696ce095691055c92b07a7f8db2b3f2a570a9c
Dejan <dejan.sheki.lukic@gmail.com>
2021-05-05 08:40:25 +0200

    Finished

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ function deleteCheck(event) {
         const todo = item.parentElement;
         //Animation
         todo.classList.add('fall');
+        removeLocalTodos(todo);
         todo.addEventListener('transitionend', function () {
             todo.remove();
         });
@@ -125,4 +126,16 @@ function getTodos() {
         //Append to list
         todoList.appendChild(todoDiv);
     });
+}
+
+function removeLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
